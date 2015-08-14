e35201907c318cceb9501ed7dd05f36cb6486c24
C. Bruce Worden <cbworden@usgs.gov>
2015-08-14 08:12:18 -0600

    First working version of BA08 module.

--- pyshake/gmpe/boore_atkinson_2008_shake.py
+++ pyshake/gmpe/boore_atkinson_2008_shake.py
@@ -16,17 +16,17 @@ class BooreAtkinson2008ShakeMap(BooreAtkinson2008):
         Calculate site corrections for sites on rock (forward=True), OR
         Remove site corrections from data on other substrates.
         '''
-        C = super.COEFFS[imt]
-        C_SR = super.COEFFS_SOIL_RESPONSE[imt]
-        pga4nl = super._get_pga_on_rock(rup, dists, C)
+        C = self.COEFFS[imt]
+        C_SR = self.COEFFS_SOIL_RESPONSE[imt]
+        pga4nl = self._get_pga_on_rock(rup, dists, C)
         if forward:
             pgm_corrected = pgm + \
-               super._get_site_amplification_linear(sites.vs30, C_SR) + \
-               super._get_site_amplification_non_linear(sites.vs30, pga4nl, C_SR)
+               self._get_site_amplification_linear(sites.vs30, C_SR) + \
+               self._get_site_amplification_non_linear(sites.vs30, pga4nl, C_SR)
         else:
             pgm_corrected = pgm - \
-               (super._get_site_amplification_linear(sites.vs30, C_SR) + \
-                super._get_site_amplification_non_linear(sites.vs30, pga4nl, C_SR))
+               (self._get_site_amplification_linear(sites.vs30, C_SR) + \
+                self._get_site_amplification_non_linear(sites.vs30, pga4nl, C_SR))
 
         return pgm_corrected
 
@@ -34,9 +34,9 @@ class BooreAtkinson2008ShakeMap(BooreAtkinson2008):
         '''
         Calculate peak ground motion on rock.
         '''
-        C = super.COEFFS[imt]
-        pgmrock = super._compute_magnitude_scaling(rup, C) + \
-                  super._compute_distance_scaling(rup, dists, C)
+        C = self.COEFFS[imt]
+        pgmrock = self._compute_magnitude_scaling(rup, C) + \
+                  self._compute_distance_scaling(rup, dists, C)
 
         return pgmrock
 
@@ -44,7 +44,7 @@ class BooreAtkinson2008ShakeMap(BooreAtkinson2008):
         '''
         Get standard deviations of GMPE.
         '''
-        C = super.COEFFS[imt]
-        stddevs = super._get_stddevs(C, stddev_types, num_sites=len(sites.vs30))
+        C = self.COEFFS[imt]
+        stddevs = self._get_stddevs(C, stddev_types, num_sites=len(sites.vs30))
         return stddevs
         
