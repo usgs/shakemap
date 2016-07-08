29c1811ca73b6393abeafdd2f32befd6591a88d4
Thompson <emthompson@usgs.gov>
2016-07-08 09:37:18 -0600

    Created initial version of mkscenariogrids

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(name='shakemap',
                 'shakemap.gmice','shakemap.directivity'],
       package_data = {'shakemap':[os.path.join('utils', 'configspec.ini')],
                       'shakemap':[os.path.join('grind', 'data', 'ps2ff', '*.csv')]},
-      scripts = ['mkfault', 'mkinputdir'],
+      scripts = ['mkfault', 'mkinputdir', 'mkscenariogrids'],
 )
