b1616d93950c0958c2ba5ed1e4b6539a80683219
C. Bruce Worden <cbworden@usgs.gov>
2016-07-19 16:32:52 -0600

    Fix travis

--- shakemap/__init__.py
+++ shakemap/__init__.py
@@ -2,5 +2,5 @@ __all__ = ["correlation", "gmice", "gmpe","grind","mapping","genex","utils","dir
 
 
 from ._version import get_versions
-__version__ = get_versions()['v0.1dev.3']
+__version__ = get_versions()['v0.1dev.4']
 del get_versions
