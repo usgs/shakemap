dd09b2d204c274120ccf42fd7d53ab3e7e81ac4b
Michael Hearne <mhearne@usgs.gov>
2016-07-28 15:49:11 -0600

    A couple of changes to manual config stuff to incorporate existence of api docs, lots of changes to makedocs, and adding makedocs to travis config.  crossing my fingers...

--- makedocs.py
+++ makedocs.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import os.path
 import shutil
+import sys
 
 DEFAULT_TAG = '0.1'
 
@@ -35,12 +36,11 @@ def main(args):
     # where should .rst files, Makefile, _build folder be written?
     SPHINX_DIR = os.path.join(os.path.expanduser('~'), '__api-doc')
 
-    # where should the temporary clone of the shakemap gh-pages repo live?
-    CLONE_DIR = os.path.join(os.path.expanduser('~'), '__shake-doc')
+    #where should the temporary clone of the shakemap gh-pages repo live?
+    CLONE_DIR=os.path.join(os.path.expanduser('~'),'__shake-doc','html')
 
     # where is the repository where this script is running?
-    REPO_DIR = os.path.dirname(os.path.abspath(
-        __file__))  # where is this script?
+    REPO_DIR = os.path.dirname(os.path.abspath(__file__))  # where is this script?
 
     # get the human friendly version of the ShakeMap version
     res, verstr, stderr = getCommandOutput('git describe')
@@ -59,67 +59,85 @@ def main(args):
     make_cmd = stdout.strip()
 
     try:
-        # clone the repository
+        #clone the repository
+        sys.stderr.write('Cloning shakemap gh-pages branch...\n')
+        if os.path.isdir(CLONE_DIR):
+            shutil.rmtree(CLONE_DIR)
         clonecmd = 'git clone -b gh-pages https://github.com/usgs/shakemap.git %s' % CLONE_DIR
-        res, stdout, stderr = getCommandOutput(clonecmd)
+        res,stdout,stderr = getCommandOutput(clonecmd)
+        if not res:
+            raise Exception('Could not clone gh-pages branch.')
 
-        # remove the current apidocs folder from that repo
-        apidocfolder = os.path.join(CLONE_DIR, 'html', 'apidoc')
+        #remove the current apidocs folder from that repo
+        apidocfolder = os.path.join(CLONE_DIR,'apidoc') #this should probably not have 'html' in the path
         if os.path.isdir(apidocfolder):
             shutil.rmtree(apidocfolder)
 
-        # run the make command to build the shakemap manual
-        makefile = os.path.join(REPO_DIR, 'doc', 'Makefile')
-        os.chdir(os.path.join(REPO_DIR, 'doc'))
+        #run the make command to build the shakemap manual
+        sys.stderr.write('Building shakemap manual (HTML)...\n')
+        makefile = os.path.join(REPO_DIR,'doc','Makefile')
+        os.chdir(os.path.join(REPO_DIR,'doc'))
         manualcmd = '%s html' % make_cmd
-        res, stdout, stderr = getCommandOutput(manualcmd)
+        res,stdout,stderr = getCommandOutput(manualcmd)
+        if not res:
+            raise Exception('Could not build the ShakeMap manual.')
+        
+        #run the sphinx api doc command
+        sys.stderr.write('Building shakemap API documentation (REST)...\n')
+        package_dir = os.path.join(REPO_DIR,'shakemap')
+        sphinx_cmd = 'sphinx-apidoc -o %s -f -l -F -H %s -A "%s" -V %s %s' % (SPHINX_DIR,PACKAGE,AUTHORS,verstr,package_dir)
+        res,stdout,stderr = getCommandOutput(sphinx_cmd)
+        if not res:
+            raise Exception('Could not build ShakeMap API documentation.')
 
-        # run the sphinx api doc command
-        package_dir = os.path.join(REPO_DIR, 'shakemap')
-        sphinx_cmd = 'sphinx-apidoc -o %s -f -l -F -H %s -A "%s" -V %s %s' % (
-            SPHINX_DIR, PACKAGE, AUTHORS, verstr, package_dir)
-        res, stdout, stderr = getCommandOutput(sphinx_cmd)
+        #run the make command to build the shakemap manual (pdf version)
+        sys.stderr.write('Building shakemap manual (PDF)...\n')
+        os.chdir(os.path.join(REPO_DIR,'doc'))
+        manualcmd = '%s latexpdf' % make_cmd
+        res,stdout,stderr = getCommandOutput(manualcmd)
+        if not res:
+            raise Exception('Could not build the PDF version of the ShakeMap manual.')
 
-        # this has created a conf.py file and a Makefile.  We need to "edit"
-        # the conf.py file to include the ReadTheDocs theme.
-        fname = os.path.join(SPHINX_DIR, 'conf.py')
-        f = open(fname, 'at')
+        #this has created a conf.py file and a Makefile.  We need to "edit" the conf.py file to include the ReadTheDocs theme.
+        fname = os.path.join(SPHINX_DIR,'conf.py')
+        f = open(fname,'at')
         f.write("sys.path.insert(0, os.path.abspath('%s'))\n" % (REPO_DIR))
         f.write("import sphinx_rtd_theme\n")
         f.write("html_theme = 'sphinx_rtd_theme'\n")
         f.write("html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]\n")
         f.close()
 
-        # Go to the sphinx directory and build the html
+        #Go to the sphinx directory and build the html
+        sys.stderr.write('Building shakemap API documentation (HTML)...\n')
         os.chdir(SPHINX_DIR)
-        res, stdout, stderr = getCommandOutput('%s html' % make_cmd)
-
-        # copy the generated content to the gh-pages branch we created earlier
-        htmldir = os.path.join(SPHINX_DIR, '_build', 'html')
+        res,stdout,stderr = getCommandOutput('%s html' % make_cmd)
+        if not res:
+            raise Exception('Could not build HTML for API documentation.')
+        
+        #copy the generated content to the gh-pages branch we created earlier
+        htmldir = os.path.join(SPHINX_DIR,'_build','html')
         if os.path.isdir(apidocfolder):
             shutil.rmtree(apidocfolder)
         shutil.copytree(htmldir, apidocfolder)
 
         if args.post:
-            # cd to directory above where html content was pushed
-            os.chdir(CLONE_DIR)
-            res, stdout, stderr = getCommandOutput('touch .nojekyll')
-            res1, stdout, stderr = getCommandOutput('git add --all')
-            res2, stdout, stderr = getCommandOutput(
-                'git commit -am"Pushing version %s to GitHub pages"' % verstr)
-            res3, stdout, stderr = getCommandOutput(
-                'git push -u origin +gh-pages')
-            if res1 + res2 + res3 < 3:
-                print(
-                    'Something bad happened when attempting to add, commit, or push gh-pages content to GitHub. Exiting.')
+            #cd to directory above where html content was pushed
+            htmldir = os.path.join(CLONE_DIR) #this should probably not have 'html' in it
+            os.chdir(htmldir)
+            res,stdout,stderr = getCommandOutput('touch .nojekyll')
+            res1,stdout,stderr = getCommandOutput('git add --all')
+            res2,stdout,stderr = getCommandOutput('git commit -am"Pushing version %s to GitHub pages"' % verstr)
+            res3,stdout,stderr = getCommandOutput('git push -u origin +gh-pages')
+            if res1+res2+res3 < 3:
+                print('Something bad happened when attempting to add, commit, or push gh-pages content to GitHub. Exiting.')
                 sys.exit(1)
+            print('You can inspect the ShakeMap manual and API docs by looking here: http://usgs.github.io/shakemap/index.html')
         else:
             if not args.clean:
-                indexpage = os.path.join(CLONE_DIR, 'html', 'index.html')
-                print(
-                    'You can inspect the ShakeMap manual and API docs by looking here: %s' % indexpage)
-    except:
-        pass
+                indexpage = os.path.join(CLONE_DIR,'index.html') #this should not have 'html' in it
+                print('You can inspect the ShakeMap manual and API docs by looking here: %s' % indexpage)
+    except Exception as error:
+        print('Error detected: %s' % str(error))
     finally:
         if args.clean:
             print('Cleaning up %s and %s created directories...' %
