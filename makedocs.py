58cf315b708cde218e8de23b0c9c35766f2361bb
Thompson <emthompson@usgs.gov>
2016-08-06 21:10:09 -0600

    Modified copytree to be recursive so that it gets contents of subdirectories.

--- makedocs.py
+++ makedocs.py
@@ -11,14 +11,18 @@ from shakemap.utils.misc import getCommandOutput
 
 DEFAULT_TAG = '0.1'
 
-# Copytree workaround from stackoverflow
+# Copytree workaround, modified from stackoverflow
 # http://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
 def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
         if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
+            # Added this to make it recursive
+            if os.path.isdir(d):
+                copytree(s, d)
+            else:
+                shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
 
@@ -235,6 +239,8 @@ def main(args):
         # earlier
         #-------------------------------------------------------------
         htmldir = os.path.join(API_DIR, '_build', 'html')
+        if not os.path.isdir(CLONE_DIR):
+            os.makedirs(CLONE_DIR)
         copytree(htmldir, CLONE_DIR)
 
         if args.post:
