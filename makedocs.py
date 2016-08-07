5858380b78003b2a9a73d1bc19c28ad209a12b4c
Thompson <emthompson@usgs.gov>
2016-08-06 20:23:00 -0600

    Added copytree workaround

--- makedocs.py
+++ makedocs.py
@@ -11,6 +11,16 @@ from shakemap.utils.misc import getCommandOutput
 
 DEFAULT_TAG = '0.1'
 
+# Copytree workaround from stackoverflow
+# http://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 def main(args):
 
@@ -225,10 +235,7 @@ def main(args):
         # earlier
         #-------------------------------------------------------------
         htmldir = os.path.join(API_DIR, '_build', 'html')
-        if os.path.isdir(CLONE_DIR):
-            shutil.copy2(htmldir, CLONE_DIR)
-        else:
-            shutil.copytree(htmldir, CLONE_DIR)
+        copytree(htmldir, CLONE_DIR)
 
         if args.post:
             #-------------------------------------------------------------
