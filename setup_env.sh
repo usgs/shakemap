42b4a360f384dc06d9bd51bee430a4f05d0678b6
Michael Hearne <mhearne@usgs.gov>
2017-06-13 10:47:14 -0600

    Fixed imports in setup_env.

--- setup_env.sh
+++ setup_env.sh
@@ -22,11 +22,28 @@ conda create --name $VENV --yes --channel conda-forge python=$PYVER ${DEPARRAY[*
 source activate shake
 
 # do pip installs of those things that are not available via conda.
+
 #grab the bleeding edge for GEM hazardlib.  They have actual releases
 #we can resort to if this becomes a problem.
-pip -v install https://github.com/gem/oq-hazardlib/archive/master.zip
-pip -v install https://github.com/usgs/MapIO/archive/master.zip
-pip -v install https://github.com/usgs/earthquake-impact-utils/archive/master.zip
+curl --max-time 60 --retry 3 -L https://github.com/gem/oq-engine/archive/master.zip -o openquake.zip
+pip -v install --no-deps openquake.zip
+rm openquake.zip
+
+#get MapIO
+curl --max-time 60 --retry 3 -L https://github.com/usgs/MapIO/archive/master.zip -o mapio.zip
+pip -v install --no-deps mapio.zip
+rm mapio.zip
+
+#get impactutils
+curl --max-time 60 --retry 3 -L https://github.com/usgs/earthquake-impact-utils/archive/master.zip -o impact.zip
+pip -v install --no-deps impact.zip
+rm impact.zip
+
+#get shakelib
+curl --max-time 60 --retry 3 -L https://github.com/usgs/shakelib/archive/master.zip -o shakelib.zip
+pip -v install --no-deps shakelib.zip
+rm shakelib.zip
+
 pip install sphinx_rtd_theme
 
 # tell the user they have to activate this environment
