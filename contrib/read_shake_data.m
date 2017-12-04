657913a4d72762e34e9d9443b998c32c62aa8cff
Michael Hearne <mhearne@usgs.gov>
2017-12-04 09:55:28 -0700

    Added two Matlab functions for reading data/metadata out of containers.

--- contrib/read_shake_data.m
+++ contrib/read_shake_data.m
@@ -1,65 +1,62 @@
-function [mean,mean_metadata,std,std_metadata] = read_shake_data(hdf_file,imt,component)
-% read_shake_data  Read ShakeMap HDF file data.
-%   [mean,mean_metadata,std,std_metadata] = read_shake_data(hdf_file,imt,component)
+function [dmean,mean_metadata,dstd,std_metadata] = read_shake_data(fname,imt,component)
+% read_shake_data  Read IMT data from a ShakeMap container.
+%   [dmean,mean_metadata,dstd,std_metadata] = read_shake_data(fname,imt,component);
 %   Inputs:
-%     - hdf_file Path to a ShakeMap HDF5 results file.
-%     - imt String intensity measure type (IMT)
-%     ('MMI','PGA','SA(1.0)',etc.)
-%     - component IMT component (usually 'Larger').
+%     - fname ShakeMap HDF file results.
+%     - imt  Intensity Measure Type (MMI,PGA,SA(0.3),etc)
+%     - component Ground motion component (Larger,Rotd50,etc.)
 %   Outputs:
-%     - mean Mean IMT value grid.
-%     - mean_metadata Mean IMT value metadata.
-%     - std Standard deviation of IMT value grid.
-%     - std_metadata Standard deviation IMT value metadata.
-    info = h5info(hdf_file);
+%     - dmean Mean IMT array.
+%     - mean_medatata Structure containing metadata for mean array.
+%       - units	Physical units of IMT or standard deviation.
+%       - digits	Number of significant digits to use for the values.
+%       - xmin	The eastern boundary of the grid (degrees longitude)
+%       - xmax	The eastern boundary of the grid (degrees longitude)
+%       - ymin	The southern boundary of the grid (degrees latitude)
+%       - ymax	The northern boundary of the grid (degrees latitude)
+%       - nx	The number of grid points in the x dimension
+%       - ny	The number of grid points in the y dimension
+%       - dx	The grid interval in the x dimension
+%       - dy	The grid interval in the y dimension
+%     - dstd Standard IMT array.
+%     - std_medatata Structure containing metadata for mean array
+%     (identical to mean_metadata).
+%
+
+    info = h5info(fname);
     for i=1:length(info.Groups)
         group = info.Groups(i);
         gname = group.Name;
-        if ~isempty(strfind(gname,'__imt'))
-            parts = strsplit(gname,'_');
-            imtstr = parts{3};
-            compstr = strip(parts{4},'_');
-            if strcmpi(imt,imtstr) && strcmpi(component,compstr)
-                
-                for j=1:length(group.Datasets)
-                    dataset = info.Groups(i).Datasets(j);
-                    dname = dataset.Name;
-                    parts = strsplit(dname,'_');
-                    quantity = strip(parts{2},'_');
-                    dsetname = sprintf('%s/%s',gname,dname);
-                    if strcmpi(quantity,'mean')
-                        mean = h5read(hdf_file,dsetname);
-                        mean_metadata = struct;
-                        for k=1:length(dataset.Attributes)
-                            attribute = dataset.Attributes(k);
-                            key = attribute.Name;
-                            if iscell(attribute.Value)
-                                value = attribute.Value{1};
-                            else
-                                value = double(attribute.Value);
-                            end
-                            mean_metadata = setfield(mean_metadata,key,value);
-                        end
-                    end
-                    if strcmpi(quantity,'std')
-                        std = h5read(hdf_file,dsetname);
-                        std_metadata = struct;
-                        for k=1:length(dataset.Attributes)
-                            attribute = dataset.Attributes(k);
-                            key = attribute.Name;
-                            if iscell(attribute.Value)
-                                value = attribute.Value{1};
-                            else
-                                value = double(attribute.Value);
-                            end
-                            std_metadata = setfield(std_metadata,key,value);
-                        end
-                    end
+        if contains(gname,imt,'IgnoreCase',1) && contains(gname,component,'IgnoreCase',1)
+            for j=1:length(group.Datasets)
+                dataset = group.Datasets(j);
+                dname = dataset.Name;
+                parts = strsplit(dname,'_');
+                quantity = parts(2);
+                %compstr = parts(4);
+                if strcmpi(quantity,'mean')
+                    [dmean,mean_metadata] = get_quantity(fname,dataset,gname);
+                elseif strcmpi(quantity,'std')
+                    [dstd,std_metadata] = get_quantity(fname,dataset,gname);
                 end
-            else
-                continue;
             end
-            
         end
-    end
+    end    
 end
+
+function [quant,metadata] = get_quantity(fname,dataset,groupname)
+    metadata = struct;
+    for k=1:length(dataset.Attributes)
+        key = dataset.Attributes(k).Name;
+        value = dataset.Attributes(k).Value;
+        if iscell(value)
+            value = value{1};
+        else
+            value = double(value);
+        end
+        metadata = setfield(metadata,key,value);
+    end
+    dsetname = sprintf('%s/%s',groupname,dataset.Name);
+    quant = h5read(fname,dsetname);
+end
+
