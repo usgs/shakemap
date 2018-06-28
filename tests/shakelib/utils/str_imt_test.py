8b7a5cfb9eac4233f012832b7f546b1ec6459242
C. Bruce Worden <cbworden@usgs.gov>
2018-06-28 10:32:57 -0600

    Revise str_imt_test to handle new lowercase file names.

--- tests/shakelib/utils/str_imt_test.py
+++ tests/shakelib/utils/str_imt_test.py
@@ -7,36 +7,36 @@ def test_imt():
     """Test the imt string functions.
 
     """
-    assert oq_to_file('SA(1.0)') == 'PSA1p0'
-    assert oq_to_file('SA(0.3)') == 'PSA0p3'
-    assert oq_to_file('SA(15.0)') == 'PSA15p0'
-    assert oq_to_file('SA(3)') == 'PSA3p0'
-    assert oq_to_file('SA(.5)') == 'PSA0p5'
+    assert oq_to_file('SA(1.0)') == 'psa1p0'
+    assert oq_to_file('SA(0.3)') == 'psa0p3'
+    assert oq_to_file('SA(15.0)') == 'psa15p0'
+    assert oq_to_file('SA(3)') == 'psa3p0'
+    assert oq_to_file('SA(.5)') == 'psa0p5'
 
     try:
         _ = oq_to_file('SA()')
     except ValueError as ve:
         assert 1 == 1
 
-    assert file_to_oq('PSA1p0') == 'SA(1.0)'
-    assert file_to_oq('PSA0p3') == 'SA(0.3)'
-    assert file_to_oq('PSA15p0') == 'SA(15.0)'
+    assert file_to_oq('psa1p0') == 'SA(1.0)'
+    assert file_to_oq('psa0p3') == 'SA(0.3)'
+    assert file_to_oq('psa15p0') == 'SA(15.0)'
 
     try:
-        _ = file_to_oq('PSA2')
+        _ = file_to_oq('psa2')
     except ValueError as ve:
         assert 1 == 1
 
     try:
-        _ = file_to_oq('PSA2p')
+        _ = file_to_oq('psa2p')
     except ValueError as ve:
         assert 1 == 1
 
     # Test that a fileimt that is the same as ['PGA', 'PGV', 'MMI']
     # is simply returned
-    assert file_to_oq('PGA') == 'PGA'
-    assert file_to_oq('PGV') == 'PGV'
-    assert file_to_oq('MMI') == 'MMI'
+    assert file_to_oq('pga') == 'PGA'
+    assert file_to_oq('pgv') == 'PGV'
+    assert file_to_oq('mmi') == 'MMI'
 
 
 if __name__ == '__main__':
