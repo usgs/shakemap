bab4b51b47d2ba71c4bd695ddad0c329b6024adb
Michael Hearne <mhearne@usgs.gov>
2018-08-16 16:11:20 -0600

    now wrapping dyfi tests with vcrpy so that we don't depend on network availability to do tests.

--- tests/shakemap/coremods/dyfi_test.py
+++ tests/shakemap/coremods/dyfi_test.py
@@ -4,22 +4,39 @@ import tempfile
 import os.path
 import shutil
 
+import vcr
 import numpy as np
 from shakemap.coremods.dyfi import _get_dyfi_dataframe
 from libcomcat.search import get_event_by_id
 
 
+def get_datadir():
+    # where is this script?
+    homedir = os.path.dirname(os.path.abspath(__file__))
+    datadir = os.path.join(homedir, '..', '..', 'data')
+    return datadir
+
+
 def test_geocoded():
     # first, test event with 10k and 1k geojson data
     eventid = 'ci14607652'
-    detail = get_event_by_id(eventid)
-    df, msg = _get_dyfi_dataframe(detail)
+    datadir = get_datadir()
+    tape_file1 = os.path.join(datadir, 'vcr_event1.yaml')
+
+    with vcr.use_cassette(tape_file1):
+        detail = get_event_by_id(eventid)
+        df, msg = _get_dyfi_dataframe(detail)
+
     np.testing.assert_almost_equal(df['INTENSITY'].sum(), 4510.1)
 
     # next, test event with only geocoded (?) resolution text data
     eventid = 'ci14745580'
-    detail = get_event_by_id(eventid)
-    df, msg = _get_dyfi_dataframe(detail)
+    tape_file2 = os.path.join(datadir, 'vcr_event2.yaml')
+
+    with vcr.use_cassette(tape_file2):
+        detail = get_event_by_id(eventid)
+        df, msg = _get_dyfi_dataframe(detail)
+
     np.testing.assert_almost_equal(df['INTENSITY'].sum(), 800.4)
 
 
@@ -27,8 +44,13 @@ def test_dyfi():
     eventid = 'nc72282711'
     try:
         tdir = tempfile.mkdtemp()
-        detail = get_event_by_id(eventid)
-        dataframe, msg = _get_dyfi_dataframe(detail)
+        datadir = get_datadir()
+        tape_file3 = os.path.join(datadir, 'vcr_event3.yaml')
+
+        with vcr.use_cassette(tape_file3):
+            detail = get_event_by_id(eventid)
+            dataframe, msg = _get_dyfi_dataframe(detail)
+
     except Exception:
         assert 1 == 2
     finally:
