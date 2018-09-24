fb6ce27da76d084ea1798b7328c2e9ff145f1b92
C. Bruce Worden <cbworden@usgs.gov>
2018-09-24 12:38:37 -0600

    Revised Wald99 GMICE to use PGA/PGV ramp function.

--- shakelib/gmice/gmice.py
+++ shakelib/gmice/gmice.py
@@ -2,12 +2,14 @@
 from abc import ABC, abstractmethod
 
 # local imports
+from openquake.hazardlib import imt
 from openquake.hazardlib.imt import PGA, PGV, SA
 
 
 class GMICE(ABC):
     """
-    Base class called to implement ground motion intensity conversion equations (GMICE).
+    Base class called to implement ground motion intensity conversion
+    equations (GMICE).
 
     Inherited by AK07, Wald99, WGRW12.
     """
@@ -50,8 +52,49 @@ class GMICE(ABC):
         """
         return self.scale
 
+    def getPreferredMI(self, df, dists=None, mag=None):
+        """
+        Function to compute macroseismic intensity from the preferred
+        ground-motion intensity. The function uses PGV by default, but
+        this may be overridden by individual classes.
+
+        Args:
+            df (dict):
+                Dictionaary containing all of the available ground
+                motions.
+            dists (ndarray):
+                Numpy array of distances from rupture (km).
+            mag (float):
+                Earthquake magnitude.
+
+        Returns:
+            ndarray of Modified Mercalli Intensity and ndarray of
+            dMMI / dln(amp) (i.e., the slope of the relationship at the
+            point in question).
+        """
+        if "PGV" not in df:
+            return None
+        oqimt = imt.from_string("PGV")
+        return self.getMIfromGM(df['PGV'], oqimt, dists, mag)[0]
+
+    def getPreferredSD(self):
+        """
+        Return an array of standard deviations for the preferred
+        ground-motion to MMI conversion. Return None is the preferred
+        IMT is not in the list of available IMTs for the GMICE.
+
+        Returns:
+            (numpy array):  Array of GM to MI sigmas (in MMI units).
+        """
+        oqimt = imt.from_string("PGV")
+        sd = self.getGM2MIsd()
+        if oqimt in sd:
+            return sd[oqimt]
+        else:
+            return None
+
     @abstractmethod
-    def getMIfromGM(cls, amps, imt, dists=None, mag=None):
+    def getMIfromGM(self, amps, imt, dists=None, mag=None):
         """
         Function to compute macroseismic intensity from ground-motion
         intensity. Supported ground-motion IMTs are PGA, PGV and PSA
@@ -74,11 +117,11 @@ class GMICE(ABC):
             ndarray of Modified Mercalli Intensity and ndarray of
             dMMI / dln(amp) (i.e., the slope of the relationship at the
             point in question).
-        """
+        """  # noqa
         pass
 
     @abstractmethod
-    def getGMfromMI(cls, mmi, imt, dists=None, mag=None):
+    def getGMfromMI(self, mmi, imt, dists=None, mag=None):
         """
         Function to tcompute ground-motion intensity from macroseismic
         intensity. Supported IMTs are PGA, PGV and PSA for 0.3, 1.0, and
@@ -100,11 +143,11 @@ class GMICE(ABC):
             Ndarray of ground motion intensity in natural log of g for PGA
             and PSA, and natural log cm/s for PGV; ndarray of dln(amp) / dMMI
             (i.e., the slope of the relationship at the point in question).
-        """
+        """  # noqa
         pass
 
     @abstractmethod
-    def getGM2MIsd(cls):
+    def getGM2MIsd(self):
         """
         Return a dictionary of standard deviations for the ground-motion
         to MMI conversion. The keys are the ground motion types.
@@ -115,7 +158,7 @@ class GMICE(ABC):
         pass
 
     @abstractmethod
-    def getMI2GMsd(cls):
+    def getMI2GMsd(self):
         """
         Return a dictionary of standard deviations for the MMI
         to ground-motion conversion. The keys are the ground motion
@@ -127,7 +170,7 @@ class GMICE(ABC):
         pass
 
     @abstractmethod
-    def _getConsts(cls, imt):
+    def _getConsts(self, imt):
         """
         Helper function to get the constants.
         """
