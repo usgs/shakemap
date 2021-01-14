bdf6096da6345019e5b08cc435b7ff154e2e46df
C. Bruce Worden <cbworden@usgs.gov>
2021-01-14 16:51:57 -0700

    Added the --dev option to transfer_pdl; cleaned up some cruft.

--- shakemap/coremods/transfer_pdl.py
+++ shakemap/coremods/transfer_pdl.py
@@ -1,6 +1,7 @@
 # stdlib imports
+import argparse
+import inspect
 import os.path
-from datetime import datetime
 import shutil
 import logging
 
@@ -52,8 +53,18 @@ class PDLTransfer(TransferBaseModule):
         # each one
         for destination, params in self.config['pdl'].items():
             params.update(properties)
-            fmt = 'Doing PDL transfer to %s...' % destination
-            logging.debug(fmt)
+            if self.usedevconfig is True:
+                if (params['devconfig'] is None or not
+                        os.path.isfile(params['devconfig'])):
+                    raise FileNotFoundError('Dev config file "%s" does not '
+                                            'exist' % params['devconfig'])
+                # Swap the config file for the devconfig file
+                params['configfile'] = params['devconfig']
+                fmt = 'Doing PDL transfer to %s DEV...' % destination
+                logging.debug(fmt)
+            else:
+                fmt = 'Doing PDL transfer to %s...' % destination
+                logging.debug(fmt)
 
             sender = PDLSender(properties=params,
                                local_directory=pdl_dir,
@@ -68,3 +79,29 @@ class PDLTransfer(TransferBaseModule):
 
         if not self.cancel:
             shutil.rmtree(downloads_dir, ignore_errors=True)
+
+    def parseArgs(self, arglist):
+        """
+        Set up the object to accept the --dev flag.
+        """
+        parser = argparse.ArgumentParser(
+            prog=self.__class__.command_name,
+            description=inspect.getdoc(self.__class__))
+        helpstr = ('Send products to the PDL server configured in "devconfig" '
+                   'in the transfer.conf configuration file rather than the '
+                   'default "configfile".')
+        parser.add_argument('-d', '--dev', help=helpstr,
+                            action='store_true', default=False)
+        #
+        # This line should be in any modules that overrides this
+        # one. It will collect up everything after the current
+        # modules options in args.rem, which should be returned
+        # by this function. Note: doing parser.parse_known_args()
+        # will not work as it will suck up any later modules'
+        # options that are the same as this one's.
+        #
+        parser.add_argument('rem', nargs=argparse.REMAINDER,
+                            help=argparse.SUPPRESS)
+        args = parser.parse_args(arglist)
+        self.usedevconfig = args.dev
+        return args.rem
